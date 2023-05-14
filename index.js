e95169f44f0a5d4f418c7578550cd61ed57f4730
Allison Tobar <allisontobarrodriguez@gmail.com>
2023-05-13 22:55:40 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = { name, email };
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        const id = object.id
+        document.body.append(id)
+
+      })
+      .catch(function(error) {
+        document.body.append(error.message)
+      });
+  }
+  
